68b9aab7b88190802f891839f0e0ca502342b25f
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2024-05-25 22:09:36 +1000

    fixed imports

--- PixTag/src/App.jsx
+++ PixTag/src/App.jsx
@@ -1,21 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
+import {
+    DeleteImage,
+    FindMatchingImage,
+    Home,
+    QueryTags,
+    QueryThumbnailUrl,
+    RegistrationPage,
+    UpdateTags,
+    UploadImage
+} from './pages'
+import { BrowserRouter, Routes, Route } from "react-dom";
+import { ChakraProvider, HStack } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
 
     return (
-        <ChakraProvider>
-            <SideBar/>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="blogs" element={<Blogs />} />
-                    <Route path="contact" element={<Contact />} />
-                    <Route path="*" element={<NoPage />} />
-                </Routes>
-            </BrowserRouter>
-        </ChakraProvider>
+        <>
+            <HStack>
+                <SideBar/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/upload" element={<UploadImage />} />
+                    </Routes>
+                </BrowserRouter>
+            </HStack>
+        </>
     )
 }
 
---
+++ PixTag/src/pages/index.jsx
@@ -0,0 +1,19 @@
+import DeleteImage from "./DeleteImage.jsx";
+import FindMatchingImage from "./FindMatchingImage.jsx";
+import Home from "./Home.jsx";
+import QueryTags from "./QueryTags.jsx";
+import QueryThumbnailUrl from "./QueryThumbnailUrl.jsx";
+import RegistrationPage from "./RegistrationPage.jsx";
+import UpdateTags from "./UpdateTags.jsx";
+import UploadImage from "./UploadImage.jsx";
+
+export {
+    DeleteImage,
+    FindMatchingImage,
+    Home,
+    QueryTags,
+    QueryThumbnailUrl,
+    RegistrationPage,
+    UpdateTags,
+    UploadImage
+};
